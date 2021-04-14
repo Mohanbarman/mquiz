ce95cc37f38d61dbc59365022a3a3318a83f4cdb
mohanbarman <mohanbarman9711@gmail.com>
2021-04-14 15:09:37 +0530

    Adding start screen

---
+++ src/screens/start.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const StartScreen: React.FC = () => {
+  return(
+      <div>
+          <h1>Welcome to the trivia</h1>
+      </div>
+  );
+};
+
+export default StartScreen;
