108b9efe763b446f84dd9cedbea8b41188137a0e
mohanbarman <mohanbarman9711@gmail.com>
2021-04-14 15:10:21 +0530

    Cleaning up

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,7 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => {
+  return <h1>Hello world</h1>;
+};
 
 export default App;
