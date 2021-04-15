589cd1bbba98efd1008a67b8e073c4f77f47f0ee
mohanbarman <mohanbarman9711@gmail.com>
2021-04-15 13:28:12 +0530

    Adding results screen

---
+++ src/screens/results.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { QuestionType } from "../api/fetchQuestions";
+
+type Props = {
+    points: number;
+    questions: QuestionType[];
+}
+
+const ResultScreen: React.FC<Props> = ({points, questions}) => {
+  return (
+    <div>
+      <h1>Results</h1>
+      <h4>You've scored {points} points out of {questions.length * 10}</h4>
+    </div>
+  );
+};
+
+export default ResultScreen;
