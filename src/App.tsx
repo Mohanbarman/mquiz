cda3451e3f113919236333780eb3821baf9bc1ba
mohanbarman <mohanbarman9711@gmail.com>
2021-04-15 13:29:21 +0530

    Adding states and methods to route between screens

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,65 @@
-import React from 'react';
+import React, { useState } from "react";
+import fetchQuestions, { QuestionType } from "./api/fetchQuestions";
+import QuestionScreen from "./screens/question";
+import ResultScreen from "./screens/results";
+import StartScreen from "./screens/start";
 
 const App: React.FC = () => {
-  return <h1>Hello world</h1>;
+  const [isGameStarted, setIsGameStarted] = useState<boolean>(false);
+  const [isGameOver, setIsGameOver] = useState<boolean>(false);
+  const [questions, setQuestions] = useState<QuestionType[]>([]);
+  const [points, setPoints] = useState<number>(0);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState<number>(0);
+
+  async function loadQuestions(
+    amount: number,
+    difficulty: string | undefined,
+    categoryID: number | undefined
+  ) {
+    const questionsResponse = await fetchQuestions(
+      amount,
+      difficulty,
+      categoryID
+    );
+    setQuestions(questionsResponse);
+    setIsGameStarted(true);
+  }
+
+  function handleNextQuestion() {
+    setCurrentQuestionIndex((p) => {
+      // game over of user is on last question
+      if (p + 2 === questions.length) {
+        setIsGameOver(true);
+        setIsGameStarted(false);
+        return 0;
+      }
+      return p + 1;
+    });
+  }
+
+  function handleAnswerSubmit(isCorrect: boolean) {
+    if (isCorrect) {
+      setPoints((p) => p + 10);
+    }
+  }
+
+  return (
+    <div>
+      {isGameStarted && (
+        <QuestionScreen
+          question={questions[currentQuestionIndex]}
+          points={points}
+          onNext={handleNextQuestion}
+          onAnswer={handleAnswerSubmit}
+          index={currentQuestionIndex}
+        />
+      )}
+      {!isGameOver && !isGameStarted && (
+        <StartScreen callback={loadQuestions} />
+      )}
+      {isGameOver && <ResultScreen points={points} questions={questions} />}
+    </div>
+  );
 };
 
 export default App;
