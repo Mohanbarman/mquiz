c2af664e0eb6e0a05490eaec6f883647f768bb89
mohanbarman <mohanbarman9711@gmail.com>
2021-04-17 16:58:41 +0530

    Adding playagain butto on results page

--- src/App/Results/index.tsx
+++ src/App/Results/index.tsx
@@ -1,21 +1,24 @@
 import React from "react";
 import { QuestionType } from "../../api/fetchQuestions";
+import Button from "../../shared/button";
 import { Flex } from "../../shared/flex";
 import { H2, H1 } from "../../shared/typography";
 
 type Props = {
   points: number;
   questions: QuestionType[];
+  onPlayAgain: () => void;
 };
 
-const ResultScreen: React.FC<Props> = ({ points, questions }) => {
+const ResultScreen: React.FC<Props> = ({ points, questions, onPlayAgain }) => {
   return (
     <Flex align={"center"} gap={"20px"}>
-      <img src="/svgs/score.svg" alt="score chart" />
+      <img src="/svgs/score.svg" alt="score chart" style={{width: '100%'}}/>
       <H2>Congratulations you’ve scored</H2>
       <H1>
         {points} out of {questions.length * 10}
       </H1>
+      <Button onClick={onPlayAgain}>Play again</Button>
     </Flex>
   );
 };
