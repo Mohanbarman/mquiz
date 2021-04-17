a654212ee7e258d9338197b6821b92e68e0dbc3a
mohanbarman <mohanbarman9711@gmail.com>
2021-04-17 10:25:21 +0530

    Adding icon and styled select

---
+++ src/shared/select.ts
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+import { colors } from './colors';
+import dropdownIcon from '../icons/dropdown.svg';
+
+
+export const SelectContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    gap: 5px;
+`
+
+export const Select = styled.select`
+    padding: 10px;
+    border-radius: 10px;
+    cursor: pointer;
+    font-size: 1.1rem;
+    font-weight: 400;
+    background-color: ${colors.light['1']};
+    border: 2px solid ${colors.dark['4']};
+    color: ${colors.dark['1']};
+    outline: none;
+    transition: all ease-in-out .2s;
+
+    /* Remove default styling (arrow) */
+    appearance: none;
+    --webkit-appearance: none;
+    --moz-appearance: none;
+
+    background-image: url('${dropdownIcon}');
+    background-repeat: no-repeat;
+    background-position: right 10px top 50%;
+
+    :hover, :focus {
+        border: 2px solid ${colors.dark['3']};
+    }
+`
