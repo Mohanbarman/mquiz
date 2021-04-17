9b4a1843b72cbfdff97fed13002cbaa494981f5b
mohanbarman <mohanbarman9711@gmail.com>
2021-04-17 12:15:02 +0530

    Adding Body1, Body2 typography

--- src/shared/typography.ts
+++ src/shared/typography.ts
@@ -9,6 +9,20 @@ export const H1 = styled.h1`
     color: ${colors.dark['1']};
 `
 
+export const Body1 = styled.p`
+    font-size: 15px;
+    font-family: 'poppins';
+    color: ${colors.dark['1']};
+    font-weight: 500;
+`
+
+export const Body2 = styled.p`
+    font-size: 13px;
+    font-family: 'poppins';
+    color: ${colors.dark['2']};
+    font-weight: 600;
+`
+
 export const Label = styled.label`
     font-size: 16px;
     font-family: 'poppins';
