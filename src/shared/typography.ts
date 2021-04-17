eb3abe5bf2bca393c63af1a80858a7c7572378f4
mohanbarman <mohanbarman9711@gmail.com>
2021-04-17 10:23:50 +0530

    Adding typography for label

--- src/shared/typography.ts
+++ src/shared/typography.ts
@@ -1,8 +1,17 @@
 import styled from 'styled-components';
+import { colors } from './colors';
 
 
 export const H1 = styled.h1`
     font-size: 45px;
     font-family: 'poppins';
     font-weight: 400;
+    color: ${colors.dark['1']};
+`
+
+export const Label = styled.label`
+    font-size: 16px;
+    font-family: 'poppins';
+    font-weight: 400;
+    color: ${colors.dark['1']};
 `
