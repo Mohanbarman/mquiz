a02436947398562332c75ef366d9d529f7e6a461
mohanbarman <mohanbarman9711@gmail.com>
2021-04-20 18:23:37 +0530

    Adding: exporting getUser, createUser resolvers

--- graphql/index.ts
+++ graphql/index.ts
@@ -4,11 +4,13 @@ import path from 'path';
 
 import addResult from './resolvers/addResult';
 import getResults from './resolvers/getResults';
+import getUsers from './resolvers/getUsers';
+import createUser from './resolvers/createUser';
 
 const typeDefsArray = loadFilesSync(path.join(__dirname, 'types'), { extensions: ['graphql'] });
 export const typeDefs = mergeTypeDefs(typeDefsArray);
 
 export const resolvers = {
-    Query: { getResults },
-    Mutation: { addResult }
+    Query: { getResults, getUsers },
+    Mutation: { addResult, createUser },
 }
