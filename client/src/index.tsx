643554bcc901bee091e0d4a98850d24567e3f825
mohanbarman <mohanbarman9711@gmail.com>
2021-04-20 19:38:23 +0530

    Adding: apollo-client in the context

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App/index';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App/index";
+import { UserProvider } from "./provider/userProvider";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/",
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <UserProvider>
+        <App />
+      </UserProvider>
+    </ApolloProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
