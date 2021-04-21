999a81cb219890c950787dd686de69c9b5f5fb54
mohanbarman <mohanbarman9711@gmail.com>
2021-04-21 12:32:21 +0530

    Adding: adding getQuestions, getCategories in resolver export, adding AllowQuestion, AllowDifficulty enum resolver map

--- graphql/index.ts
+++ graphql/index.ts
@@ -3,14 +3,29 @@ import { mergeTypeDefs } from '@graphql-tools/merge';
 import path from 'path';
 
 import addResult from './resolvers/addResult';
+import createUser from './resolvers/createUser';
+
 import getResults from './resolvers/getResults';
 import getUsers from './resolvers/getUsers';
-import createUser from './resolvers/createUser';
+import getQuestions from './resolvers/getQuestions'
+import getCategories from './resolvers/getCategories';
+
 
 const typeDefsArray = loadFilesSync(path.join(__dirname, 'types'), { extensions: ['graphql'] });
 export const typeDefs = mergeTypeDefs(typeDefsArray);
 
 export const resolvers = {
-    Query: { getResults, getUsers },
+    Query: { getResults, getUsers, getQuestions, getCategories },
     Mutation: { addResult, createUser },
+
+    // enum resolvers map
+    AllowedQuestion: {
+        BOOLEAN: 'boolean',
+        MULTIPLE: 'multiple',
+    },
+    AllowedDifficulty: {
+        HARD: 'hard',
+        EASY: 'easy',
+        MEDIUM: 'medium',
+    }
 }
