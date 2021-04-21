6cc52cd1420808e09a8337912d9ecdf9bc6ec85c
mohanbarman <mohanbarman9711@gmail.com>
2021-04-21 22:19:08 +0530

    Removing: unused imports and unused img

--- client/src/App/Results/index.tsx
+++ client/src/App/Results/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { QuestionType } from "../../api/fetchQuestions";
+
 import Button from "../../shared/button";
 import { Flex } from "../../shared/flex";
 import { H2, H1 } from "../../shared/typography";
-import resultSvg from '../../assets/score.svg';
+import resultSvg from "../../assets/score.svg";
+import { QuestionType } from "../../types";
 
 type Props = {
   points: number;
@@ -14,7 +15,7 @@ type Props = {
 const ResultScreen: React.FC<Props> = ({ points, questions, onPlayAgain }) => {
   return (
     <Flex align={"center"} gap={"20px"}>
-      <img src={resultSvg} alt="score chart" style={{width: '100%'}}/>
+      <img src={resultSvg} alt="score chart" style={{ width: "100%" }} />
       <H2>Congratulations you’ve scored</H2>
       <H1>
         {points} out of {questions.length * 10}
