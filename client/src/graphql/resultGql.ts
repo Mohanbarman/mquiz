b8d09dd2d55c73d60d45c74d4755bf3a1046bca5
mohanbarman <mohanbarman9711@gmail.com>
2021-04-22 20:32:30 +0530

    Adding: addResult and getResult gql

---
+++ client/src/graphql/resultGql.ts
@@ -0,0 +1,31 @@
+import { gql } from '@apollo/client';
+
+export const ADD_RESULT = gql`
+    mutation ADD_RESULT($category: String!, $userID: String!, $difficulty: String!, $totalQuestions: Int!, $rightQuestions:Int!) {
+        addResult(resultInput: {
+            category: $category,
+            userID: $userID,
+            difficulty: $difficulty,
+            totalQuestions: $totalQuestions,
+            rightQuestions: $rightQuestions,
+        }) {
+            id
+            user { id name }
+            totalQuestions
+            rightQuestions
+            category
+            createdAt
+        }
+    }
+`
+
+export const GET_RESULTS = gql`
+    query GET_RESULTS {
+        id
+        user { id name }
+        totalQuestions
+        rightQuestions
+        category
+        createAt
+    }
+`
