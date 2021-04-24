417f3f63312c4c5315e4865eb5b3270010adbf60
mohanbarman <mohanbarman9711@gmail.com>
2021-04-24 13:28:25 +0530

    Removing: category from results table

--- client/src/App/Results/index.tsx
+++ client/src/App/Results/index.tsx
@@ -31,13 +31,12 @@ type ResultType = {
 
 const ResultScreen: React.FC<Props> = ({ points, questions, onPlayAgain }) => {
   const { user } = (useContext(UserContext) as unknown) as UserContextType;
-  const resultHeaders = ["Name", "Time", "Category", "Score"];
+  const resultHeaders = ["Name", "Time", "Score"];
   const [results, setResults] = useState<ResultType[]>();
 
   const resultBody = results?.map((i) => [
     i.user.id === user.id ? "You" : i.user.name,
     moment(parseInt(i.createdAt)).fromNow(),
-    i.category,
     `${Math.floor((i.rightQuestions / i.totalQuestions) * 100)}%`,
   ]);
 
@@ -50,7 +49,6 @@ const ResultScreen: React.FC<Props> = ({ points, questions, onPlayAgain }) => {
   useEffect(() => {
     if (resultData) {
       setResults(resultData.getResults);
-      console.log(resultData.getResults);
     }
   }, [resultData]);
 
@@ -61,7 +59,8 @@ const ResultScreen: React.FC<Props> = ({ points, questions, onPlayAgain }) => {
         {points} out of {questions.length * 10}
       </H1>
       <Button onClick={onPlayAgain}>Play again</Button>
-      <H2 style={{ textAlign: "center", margin: '30px 0' }}>SCOREBOARD</H2>
+      <H2 style={{ textAlign: "center", margin: "30px 0" }}>SCOREBOARD</H2>
+      {resultsError && <span>{resultsError}</span>}
       {resultBody && !resultsLoading ? (
         <Table headers={resultHeaders} body={resultBody} />
       ) : (
