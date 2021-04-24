330b1eb91e030487ac0045b43d85f5e16ee3333e
mohanbarman <mohanbarman9711@gmail.com>
2021-04-24 13:27:09 +0530

    Adding: sorting by createAt in results

--- graphql/resolvers/getResults.ts
+++ graphql/resolvers/getResults.ts
@@ -1,7 +1,7 @@
 import ResultModel from '../../models/resultModel';
 
-const getResult = async (parent: any) => {
-    const results = await ResultModel.find().populate('user').exec();
+const getResult = async () => {
+    const results = await ResultModel.find().sort({createdAt: -1}).populate('user').exec();
     return results;
 }
 
